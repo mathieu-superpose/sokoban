11c5686a38862fdf81099a207cd51798fafc91a7
Mathieu Joly <mathieujoly@MacBook-Pro-de-Mathieu.local>
2022-09-10 17:06:56 +0200

    add animated character

--- src/player.js
+++ src/player.js
@@ -1,46 +1,61 @@
 import * as PIXI from "pixi.js";
+import { Container } from '@pixi/display';
 
 const PLAYER_SPEED = 3;
 const PLAYER_SCALE = 2;
 
 export function addPlayer(app) {
-  const player = getPlayer();
-  app.stage.addChild(player.sprite);
-  const { sprite: playerSprite, inputs: playerInputs } = player;
-  player.sprite.x = app.view.width / 2;
-  player.sprite.y = app.view.height - 150;
+  const player = getPlayer(app);
+
+  player.container.x = app.view.width / 2;
+  player.container.y = app.view.height - 150;
+  app.stage.addChild(player.container);
+
+  const { container: playerContainer, inputs: playerInputs } = player;
 
   app.ticker.add((timeDelta) => {
-    if (playerInputs.has("moveUp"))
-      playerSprite.y -= timeDelta * PLAYER_SPEED;
-    if (playerInputs.has("moveDown"))
-      playerSprite.y += timeDelta * PLAYER_SPEED;
+
+    if (playerInputs.has("moveUp")) {
+        playerContainer.y -= timeDelta * PLAYER_SPEED;
+        //animation.x -= timeDelta * PLAYER_SPEED;
+    }
+    if (playerInputs.has("moveDown")) {
+        playerContainer.y += timeDelta * PLAYER_SPEED;
+        //animation.x += timeDelta * PLAYER_SPEED;
+    }
     if (playerInputs.has("moveLeft"))
-      playerSprite.x -= timeDelta * PLAYER_SPEED;
+    playerContainer.x -= timeDelta * PLAYER_SPEED;
+
     if (playerInputs.has("moveRight"))
-      playerSprite.x += timeDelta * PLAYER_SPEED;
+    playerContainer.x += timeDelta * PLAYER_SPEED;
+
 
     // clamp player to screen
-    playerSprite.x = Math.max(0, (Math.min(playerSprite.x, app.view.width - playerSprite.width)))
-    playerSprite.y = Math.max(0, (Math.min(playerSprite.y, app.view.height - playerSprite.height)))
+    playerContainer.x = Math.max(0, (Math.min(playerContainer.x, app.view.width - playerContainer.width)))
+    playerContainer.y = Math.max(0, (Math.min(playerContainer.y, app.view.height - playerContainer.height)))
   });
 
   return player;
 }
 
-function getPlayer() {
-  return {
-    id: (Math.random() * 1000000).toFixed(),
-    sprite: getPlayerSprite(),
-    inputs: getPlayerInputs(),
-  };
-}
+function getPlayer(app) {
+    const player = {
+        id: (Math.random() * 1000000).toFixed(),
+        container: new Container(),
+        inputs: getPlayerInputs(),
+    };
 
-function getPlayerSprite() {
-  const playerSprite = PIXI.Sprite.from("character-front.png");
-  playerSprite.transform.scale._x = PLAYER_SCALE
-  playerSprite.transform.scale._y = PLAYER_SCALE
-  return playerSprite;
+    app.loader
+    .add('assets/character/character-walk.json')
+    .load((loader, resources) => {
+        const walkFrontTextures = resources['assets/character/character-walk.json'].data.animations['character-walk-front'].map(img => PIXI.Texture.from(img))
+        const walkFrontAnim = new PIXI.AnimatedSprite(walkFrontTextures);
+        walkFrontAnim.animationSpeed = 0.15;
+        walkFrontAnim.play();
+        player.container.addChild(walkFrontAnim);
+    });
+    
+    return player
 }
 
 const keyToMoveMap = {
